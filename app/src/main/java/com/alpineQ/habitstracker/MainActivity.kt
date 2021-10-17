8ab1dc3f61410b2b19ce050dc8f29bb28c841fe7
alpineQ <mk_dev@mail.ru>
2021-10-17 23:11:39 +0300

    Добавлено уведомление при запуске приложения

--- app/src/main/java/com/alpineQ/habitstracker/MainActivity.kt
+++ app/src/main/java/com/alpineQ/habitstracker/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.alpineQ.habitstracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import java.util.*
 
 
 private const val TAG = "MainActivity"
+private const val CHANNEL_ID = "Greetings notification"
+
 class MainActivity : AppCompatActivity(), HabitListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +29,48 @@ class MainActivity : AppCompatActivity(), HabitListFragment.Callbacks {
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
+
+        createNotificationChannel()
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Welcome to Habits Tracker!")
+            .setContentText("Start by creating new habit")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(Random().nextInt(), builder.build())
+        }
+
     }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
     override fun onHabitSelected(habitID: UUID) {
         val fragment = HabitFragment.newInstance(habitID)
         supportFragmentManager
